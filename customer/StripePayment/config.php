483f394615b0d9860dc58b33e06ad9fd7c3d7502
Amrit Raj <149646457+oscel0t@users.noreply.github.com>
2025-02-02 15:48:43 +0530

    Update config.php

--- customer/StripePayment/config.php
+++ customer/StripePayment/config.php
@@ -1,15 +1,11 @@
 <?php
-	require_once "stripe-php-master/init.php";
-	require_once "products.php";
+require_once "stripe-php-master/init.php";
+require_once "products.php";
 
-$stripeDetails = array(
-		"secretKey" => "sk_test_xxx",  //Your Stripe Secret key
-		"publishableKey" => "pk_test_xxx"  //Your Stripe Publishable key
-	);
+$stripeDetails = [
+    "secretKey" => "sk_test_51QnRMf2fOrBn3yViEAcQMZcPhX0BTiazQzCKSEdBbdJ1YuTHhMif3ET5mqXoJRs0nzi0Rzl2LMMCJo3msWrdHNpX00siG4fm3e", // Replace with your Stripe Secret key
+    "publishableKey" => "pk_test_51QnRMf2fOrBn3yViXnXuOxDcwEZeDQVCXm1lJBnm71WGJWeqy1pjez5WohuxmBRb3QnhPHCYmhq8iZHagqyhtBtY00tEuNf26V" // Replace with your Stripe Publishable key
+];
 
-	// Set your secret key: remember to change this to your live secret key in production
-	// See your keys here: https://dashboard.stripe.com/account/apikeys
-	\Stripe\Stripe::setApiKey($stripeDetails['secretKey']);
-
-	
+\Stripe\Stripe::setApiKey($stripeDetails['secretKey']);
 ?>
